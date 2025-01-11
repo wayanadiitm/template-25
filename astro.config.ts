ba13e5ee4ffc129a8183567cef56bcfa15899c23
wayanad house <150238196+wayanadiitm@users.noreply.github.com>
2025-01-11 19:57:57 +0530

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   // used to generate images
   site:
     process.env.VERCEL_ENV === 'production'
-      ? 'https://wayanadiitm.github.io/template-25'
+      ? 'https://wayanadiitm.github.io'
       : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/`
       : 'https://localhost:3000/',
