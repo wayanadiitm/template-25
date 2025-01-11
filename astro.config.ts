5175348eac6d04be1c1c4f8bdf8e9e3c87f08333
wayanad house <150238196+wayanadiitm@users.noreply.github.com>
2025-01-11 20:00:46 +0530

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -4,13 +4,9 @@ import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://wayanadiitm.github.io'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
-  trailingSlash: 'ignore',
+  site: 'https://wayanadiitm.github.io',
+  base: 'template-25',
+  //trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
     optimizeDeps: {
